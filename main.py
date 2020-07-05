b4026603955b046d28942c7f9e6be4ea264618d8
felipe-nogueira <felipe.nogueira@stone.com.br>
2020-07-05 02:06:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
-import os
 from flask import Flask
-from flask_cors import CORS
-from services import Mysql
-from services import CustomJSONEncoder
-from api import setup_blueprint as pizza_blueprint
-
-
 app = Flask(__name__)
 
-app.json_encoder = CustomJSONEncoder
-CORS(app)
-
-
-def _register_blueprints(app):
-    print("Registering blueprints")
-    app.register_blueprint(pizza_blueprint())
-
-def _start_services():
-    print("Starting services")
-    Mysql()
-    print("Services started")
-
-
-def start_app(app):
-    _start_services()
-    _register_blueprints(app)
-
-
-def run_server():
-    port = int(os.environ.get("PORT", 5000))
-
-    _register_blueprints(app)
-
-    app.run(host='0.0.0.0', port=port)
+@app.route("/")
+def hello():
+    return "Hello World!"
