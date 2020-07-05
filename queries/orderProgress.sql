31ff12eed5c335e6c795b72da2c4706d0853d2db
felipe-nogueira <felipe.nogueira@stone.com.br>
2020-07-05 06:12:45 -0300

    Update orderProgress.sql

--- queries/orderProgress.sql
+++ queries/orderProgress.sql
@@ -1,6 +1,9 @@
 SELECT
-    *
-from `Order-Progress`
+    op.*,
+    round(p.price * op.quantity,2) as price
+from `Order-Progress` as op
+inner join Products p ON
+	p.name = op.productName
 WHERE
     userName = '{0}' and
     DATE_FORMAT(now(),'%Y-%M-%D') = DATE_FORMAT(date,'%Y-%M-%D')
